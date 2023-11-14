b0a2d76b16145e78e64a55465f9345b34925f1ca
Hoa Bui <hoabui8323@gmail.com>
2023-11-14 11:23:29 +0700

    Thêm hieu ung bai van giu nguyen code + file output

--- baiKiemTra_14.11.php
+++ baiKiemTra_14.11.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<title>Bài Kiểm Tra</title>
-<header align = "center" style="font-size: 25px">---<b> Bài Kiểm Tra </b>---</header> <br>
+<title>Bài Kiểm Tra 14.11 </title>
+<header align = "center" style="font-size: 25px">---<b> Bài Kiểm Tra 14.11 </b>---</header> <br>
 
 <body>
     <table align = "center" >
         <tr><td><b>Họ và tên: Bùi Thị Hồng Hoa</b></td></tr>
         <tr><td><b>STT: 11</b></td></tr>
-        <tr><td><b>MSV: 21D19114</b></td></tr>
+        <tr><td><b>MSV: 21D191144</b></td></tr>
     </table>
     
     <hr>
@@ -24,6 +24,16 @@
     
     <fieldset>
         <legend align = "center"><b> Bài 1 </b>:</legend>
+
+        <b>Đề bài:</b> <br> <i>Viết một chương trình PHP để hiển thị dãy số Fibonacci. Yêu cầu:<br>
+        <ul>
+            <li>Tạo một hàm generateFibonacci nhận một số nguyên dương n và trả về dãy số Fibonacci đầu tiên có n phần tử.</li>
+            <br>
+            
+            <li>Hiển thị dãy số Fibonacci đầu tiên có 10 phần tử.</li>
+        </ul></i>
+
+
         <b>Output:</b> <br>
 
         <?php 
@@ -42,7 +52,7 @@
 
         $n = 10;
         $a =generateFibonacci($n);
-        echo "Dãy số Fibonacci đầu tiên có  $n phần tử: <br>";
+        echo "<i>Dãy số Fibonacci đầu tiên có  $n phần tử: <br></i>";
             foreach ($a as $i) {
                 echo $i . " ";
             }
@@ -56,43 +66,71 @@
 
     <fieldset >
         <legend align = "center"><b> Bài 2: </b></legend>
-        <b>Output:</b> <br>
+        <b>Đề bài:</b><br>
+        <i>
+        Viết một ứng dụng PHP để quản lý thông tin về học sinh trong một lớp học sử dụng mảng kết hợp. Yêu cầu:
+        <ul>
+            <li>Tạo một mảng kết hợp chứa thông tin về học sinh với các khóa như id, name, age, và grade.</li>
+            <li>Hiển thị thông tin của tất cả học sinh trong mảng.</li>
+            <li>Viết hàm để tìm học sinh có điểm cao nhất (grade).</li>
+        </ul>
+        </i>
+
+        <b>Output:</b> <br> <br>
         <?php 
+        // Tạo một mảng kết hợp chứa thông tin về học sinh với các khóa như id, name, age, và grade.
             $hocSinh = [
               [
-                "id" => 1,
-                "name" => "HoaBui",
+                "id" => 10,
+                "name" => "Nguyễn Thị Hiên",
                 "age"=> 20,
-                "grade" => 80,
+                "grade" => 90,
               ],
               [
-                "id" => 2,
-                "name" => "Hoapham",
+                "id" => 11,
+                "name" => "Bùi Thị Hồng Hoa",
                 "age"=> 20,
-                "grade" => 100,
+                "grade" => 91,
               ],
               [
-                "id" => 5,
-                "name" => "HoaLinh",
+                "id" => 12,
+                "name" => "Nguyễn Ngọc Huy",
                 "age"=> 20,
-                "grade" => 200,
+                "grade" => 92,
+              ],
+              [
+                "id" => 13,
+                "name" => "Nguyễn Thị Mỹ Huyền",
+                "age"=> 20,
+                "grade" => 93,
+              ],
+              [
+                "id" => 31,
+                "name" => "Thân Quang Trung",
+                "age"=> 20,
+                "grade" => 94,
               ],
 
             ];
-           
-            echo " Thông tin của tất cả học sinh:<br>";
+
+        //Hiển thị thông tin của tất cả học sinh trong mảng.
+            echo " <i>Thông tin của tất cả học sinh: </i><br>";
             foreach ($hocSinh as $hs) {
-                echo "ID: " . $hs['id'] . ", ";
-                echo "Tên: " . $hs['name'] . ", ";
-                echo "Tuổi: " . $hs['age'] . ", ";
-                echo "Điểm: " . $hs['grade'] . "";
+                echo "-<br>";
+                echo "ID: " . $hs["id"] . "<br>";
+                echo "Tên: " . $hs["name"] . "<br>";
+                echo "Tuổi: " . $hs["age"] . "<br>";
+                echo "Điểm: " . $hs["grade"] . "";
                 echo "<br>";
             }
-             echo "----------<br>";
+             echo "<br>----------<br><br>";
+
+        // Viết hàm để tìm học sinh có điểm cao nhất (grade).
             function timDiemCao($hocSinh) {
                 $diemCaoNhat = 0;
                 $hocSinhDiemCao = null;
                 foreach ($hocSinh as $hs) {
+                    
                     if ($hs["grade"] > $diemCaoNhat) {
                         $diemCaoNhat = $hs["grade"];
                         $hocSinhDiemCao = $hs;
@@ -101,19 +139,14 @@
                 return $hocSinhDiemCao;
             }
             
-            
             $hocSinhCaoNhat = timDiemCao($hocSinh);
-
-            echo "Học sinh có điểm cao nhất:<br>";
-            echo "ID: " . $hocSinhCaoNhat['id'] . ", Name: " . $hocSinhCaoNhat['name'] . ", Age: " . $hocSinhCaoNhat['age'] . ", Grade: " . $hocSinhCaoNhat['grade'] . "<br>";
+            echo "<i>Học sinh có điểm cao nhất:<br></i>-<br>";
+            echo "ID: " . $hocSinhCaoNhat["id"] . "<br>";
+            echo "Tên: " . $hocSinhCaoNhat["name"] . "<br> " ;
+            echo "Tuổi: " . $hocSinhCaoNhat["age"] . "<br> " ;
+            echo "Điểm: " . $hocSinhCaoNhat["grade"] . "<br>" ;
 
         ?>
-
         <br>
-        
-
     </fieldset>
-  
-    
-   
 </body
