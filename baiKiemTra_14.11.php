aea677e755e34c2b88200c1377ce2afdf70a93fc
Hoa Bui <hoabui8323@gmail.com>
2023-11-14 08:43:00 +0700

    Them file bai tap

---
+++ baiKiemTra_14.11.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<title>Bài Kiểm Tra</title>
+<header align = "center" style="font-size: 25px">---<b> Bài Kiểm Tra </b>---</header> <br>
+
+<body>
+    <table align = "center" >
+        <tr><td><b>Họ và tên: Bùi Thị Hồng Hoa</b></td></tr>
+        <tr><td><b>STT: 11</b></td></tr>
+        <tr><td><b>MSV: 21D19114</b></td></tr>
+    </table>
+    
+    <hr>
+  
+    <fieldset>
+        <legend align = "center"><b> Trắc nghiệm </b></legend>
+        1. <br>
+        2. <br>
+        3. <br>
+        4. <br>
+        5. <br>
+    </fieldset>
+
+    <br>
+    
+    <fieldset>
+        <legend align = "center"><b> Bài 1 </b>:</legend>
+        <b>Đề bài:</b> <br>
+        <b>Code:</b> <br>
+        
+            function chiaHet($a){<br> &nbsp;
+                if($a%2==0){ <br> &ensp; &ensp; 
+                    echo "Số $a là Số Chẵn";<br>
+                }else{ echo "Số $a là Số lẻ"; }  <br>&nbsp;
+            }<br>
+       
+
+        <?php 
+       
+        ?>
+
+        <br>
+        <b>Output:</b>
+    </fieldset>
+    
+    <br>
+
+    <fieldset >
+        <legend align = "center"><b> Bài 2: </b></legend>
+        <b>Đề bài:</b> <br>
+        <b>Code:</b> <br>
+        
+            function chiaHet($a){<br> &nbsp;
+                if($a%2==0){ <br> &ensp; &ensp; 
+                    echo "Số $a là Số Chẵn";<br>
+                }else{ echo "Số $a là Số lẻ"; }  <br>&nbsp;
+            }<br>
+       
+
+        <?php 
+        
+        ?>
+
+        <br>
+        <b>Output:</b>
+
+    </fieldset>
+  
+    
+   
+</body
